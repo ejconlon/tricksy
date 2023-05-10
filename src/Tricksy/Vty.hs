9bc31dcb0ba7ceba5bdda9e2dbe0783d9f59e124
Eric Conlon <ejconlon@gmail.com>
2023-05-10 16:33:55 -0700

    etc

--- src/Tricksy/Vty.hs
+++ src/Tricksy/Vty.hs
@@ -5,16 +5,27 @@
 -- TODO ui events
 module Tricksy.Vty where
 
+import Control.Applicative (Alternative (..), liftA2)
 import Control.Exception (bracket)
-import Control.Monad (void, (<=<))
-import Control.Monad.Reader (MonadReader (..), asks, runReader)
+import Control.Monad (void, when, (<=<))
+import Control.Monad.Reader (MonadReader (..), ReaderT (..), asks, runReader)
+import Control.Monad.State.Strict (MonadState (..), State, StateT (..), gets, modify', runState)
+import Control.Monad.Trans (MonadTrans (..))
+import Control.Monad.Trans.Maybe (MaybeT (..))
 import Data.Bifoldable (Bifoldable (..))
 import Data.Bifunctor (Bifunctor (..))
 import Data.Bitraversable (Bitraversable (..))
 import Data.Foldable (foldl', toList)
 import Data.List (intersperse)
-import Data.List.NonEmpty (NonEmpty (..))
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
 import Data.Monoid (Sum (..))
+import Data.Sequence (Seq (..))
+import Data.Sequence qualified as Seq
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Data.Text (Text)
+import Data.Text.Encoding qualified as TE
 import Graphics.Vty qualified as V
 import Lens.Micro (Lens', lens)
 
@@ -91,23 +102,23 @@ boxSize (Box _ (V2 tlx tly) (V2 cx cy) (V2 brx bry)) =
 
 data Elem b r z
   = ElemPart !z
-  | ElemLayout !Layout !b !(NonEmpty r)
+  | ElemLayout !Layout !b !r !(Seq r)
   deriving stock (Eq, Ord, Show, Functor, Foldable, Traversable)
 
 instance Bifunctor (Elem b) where
   bimap f g = \case
     ElemPart z -> ElemPart (g z)
-    ElemLayout ly bo rs -> ElemLayout ly bo (fmap f rs)
+    ElemLayout ly bo r rs -> ElemLayout ly bo (f r) (fmap f rs)
 
 instance Bifoldable (Elem b) where
   bifoldr f g x = \case
     ElemPart z -> g z x
-    ElemLayout _ _ rs -> foldr f x rs
+    ElemLayout _ _ r rs -> f r (foldr f x rs)
 
 instance Bitraversable (Elem b) where
   bitraverse f g = \case
     ElemPart z -> fmap ElemPart (g z)
-    ElemLayout ly bo rs -> fmap (ElemLayout ly bo) (traverse f rs)
+    ElemLayout ly bo r rs -> liftA2 (ElemLayout ly bo) (f r) (traverse f rs)
 
 data FrameF n r = FrameF {frameBoxF :: !(Box n), frameContentF :: !r}
   deriving stock (Eq, Ord, Show, Functor, Foldable, Traversable)
@@ -119,7 +130,7 @@ newtype Frame n b z = Frame {unFrame :: FrameF n (Elem b (Frame n b z) z)}
 frame :: (SizeLike n, HasSize n z, HasFill n b) => Elem b (Frame n b z) z -> Frame n b z
 frame = \case
   ElemPart z -> framePart z
-  ElemLayout ly bo fs -> cat ly bo fs
+  ElemLayout ly bo f fs -> cat ly bo f fs
 
 frameBox :: Frame n b z -> Box n
 frameBox = frameBoxF . unFrame
@@ -157,7 +168,7 @@ frameBind f = frameCata (elemBind f . frameContentF)
 elemBind :: (SizeLike n, HasSize n w, HasFill n b) => (z -> Frame n b w) -> Elem b (Frame n b w) z -> Frame n b w
 elemBind f = \case
   ElemPart z -> f z
-  ElemLayout ly bo fs -> cat ly bo fs
+  ElemLayout ly bo r rs -> cat ly bo r rs
 
 newtype ImBorder = ImBorder {unImBorder :: Bool}
   deriving stock (Show)
@@ -197,7 +208,7 @@ instance Semigroup n => HasSize n (Box n) where
 instance (Monoid n, Ord n, HasSize n r, HasSize n z, HasFill n b) => HasSize n (Elem b r z) where
   getSize = \case
     ElemPart z -> getSize z
-    ElemLayout l b as -> cat l b (fmap getSize as)
+    ElemLayout l b a as -> cat l b (getSize a) (fmap getSize as)
 
 instance (Semigroup n, HasSize n a) => HasSize n (Frame n b a) where
   getSize = getSize . frameBoxF . unFrame
@@ -261,11 +272,11 @@ class Stackable b a where
   vstack = stack LayoutVert
   stack :: Layout -> b -> a -> a -> a
   stack = \case LayoutHoriz -> hstack; LayoutVert -> vstack
-  hcat :: b -> NonEmpty a -> a
-  hcat bo (hd :| tl) = foldl' (hstack bo) hd tl
-  vcat :: b -> NonEmpty a -> a
-  vcat bo (hd :| tl) = foldl' (vstack bo) hd tl
-  cat :: Layout -> b -> NonEmpty a -> a
+  hcat :: b -> a -> Seq a -> a
+  hcat = foldl' . hstack
+  vcat :: b -> a -> Seq a -> a
+  vcat = foldl' . vstack
+  cat :: Layout -> b -> a -> Seq a -> a
   cat = \case LayoutHoriz -> hcat; LayoutVert -> vcat
   {-# MINIMAL stack | (hstack, vstack) #-}
 
@@ -279,8 +290,8 @@ instance (Monoid n, Ord n, HasFill n b) => Stackable b (V2 n) where
 instance Stackable V.Image V.Image where
   hstack bo im1 im2 = V.horizCat [im1, bo, im2]
   vstack bo im1 im2 = V.vertCat [im1, bo, im2]
-  hcat bo = V.horizCat . intersperse bo . toList
-  vcat bo = V.vertCat . intersperse bo . toList
+  hcat bo hd tl = V.horizCat (intersperse bo (hd : toList tl))
+  vcat bo hd tl = V.vertCat (intersperse bo (hd : toList tl))
 
 instance (Monoid n, Ord n, HasFill n b) => Stackable b (Box n) where
   stack ly bo b1 b2 = Box defBoxAlign mempty (stack ly bo (getSize b1) (getSize b2)) mempty
@@ -292,27 +303,34 @@ instance (SizeLike n, HasSize n z, HasFill n b) => Stackable b (Frame n b z) whe
         f1' = Frame (FrameF b1' c1)
         f2' = Frame (FrameF b2' c2)
         b = stack ly bo b1' b2'
-        c = ElemLayout ly bo (f1' :| [f2'])
+        c = ElemLayout ly bo f1' (Seq.singleton f2')
     in  Frame (FrameF b c)
 
 build :: (Semigroup n, Stackable c w) => (n -> Layout -> b -> c) -> (Box n -> z -> w) -> Frame n b z -> w
 build g f = frameCata $ \(FrameF bx e) ->
   case e of
     ElemPart z -> f bx z
-    ElemLayout ly bo ws ->
+    ElemLayout ly bo w ws ->
       let n = case ly of LayoutHoriz -> getHeight bx; LayoutVert -> getWidth bx
-      in  cat ly (g n ly bo) ws
+      in  cat ly (g n ly bo) w ws
 
 buildM :: (Monad m, Semigroup n, Stackable c w) => (n -> Layout -> b -> m c) -> (Box n -> z -> m w) -> Frame n b z -> m w
 buildM g f = frameCataM $ \(FrameF bx e) ->
   case e of
     ElemPart z -> f bx z
-    ElemLayout ly bo ws ->
+    ElemLayout ly bo w ws ->
       let n = case ly of LayoutHoriz -> getHeight bx; LayoutVert -> getWidth bx
-      in  fmap (\c -> cat ly c ws) (g n ly bo)
+      in  fmap (\c -> cat ly c w ws) (g n ly bo)
 
 hitBox :: (Semigroup n, Ord n) => V2 n -> Box n -> Bool
 hitBox (V2 hr hc) (Box _ (V2 tlr tlc) (V2 nr nc) _) = hr >= tlr && hr < tlr <> nr && hc >= tlc && hc < tlc <> nc
+
+layoutOffset :: (Group n, HasFill n b) => Box n -> Layout -> b -> V2 n
+layoutOffset bx ly bo =
+  let z = getFill bo
+  in  case ly of
+        LayoutHoriz -> V2 (invert (getWidth bx <> z)) mempty
+        LayoutVert -> V2 mempty (invert (getHeight bx <> z))
 
 -- | Find the innermost hit
 hitFrame :: (Group n, Ord n, HasFill n b) => V2 n -> Frame n b z -> Maybe z
@@ -321,21 +339,14 @@ hitFrame initPos fr = goRoot
   goRoot = flip runReader initPos $ flip frameCata fr $ \(FrameF bx con) ->
     case con of
       ElemPart z -> asks (\pos -> if hitBox pos bx then Just z else Nothing)
-      ElemLayout ly bo fs -> goFold bx ly bo mempty (toList fs)
-  goFold bx ly bo !off = \case
+      ElemLayout ly bo r rs -> goFold (layoutOffset bx ly bo) mempty (r : toList rs)
+  goFold layOff !off = \case
     [] -> pure Nothing
-    f : fs -> do
-      mz <- local (off <>) f
+    r : rs -> do
+      mz <- local (off <>) r
       case mz of
         Just _ -> pure mz
-        Nothing -> do
-          let off' = off <> adjustOff bx ly bo
-          goFold bx ly bo off' fs
-  adjustOff bx ly bo =
-    let z = getFill bo
-    in  case ly of
-          LayoutHoriz -> V2 (invert (getWidth bx <> z)) mempty
-          LayoutVert -> V2 mempty (invert (getHeight bx <> z))
+        Nothing -> goFold layOff (off <> layOff) rs
 
 type ImFrame = Frame (Sum Int) ImBorder V.Image
 
@@ -365,9 +376,31 @@ testI at =
         (vstack bo (framePart (V.string at "hello")) (framePart (V.string at "world")))
         (framePart (V.string at ":)"))
 
+data Event n
+  = EvKey !V.Key !(Set V.Modifier)
+  | EvMouseDown !(V2 n) !V.Button !(Set V.Modifier)
+  | EvMouseUp !(V2 n)
+  | EvResize !(V2 n)
+  | EvPaste !Text
+  | EvLostFocus
+  | EvGainedFocus
+  deriving stock (Eq, Ord, Show)
+
+convertEv :: V.Event -> Event (Sum Int)
+convertEv = \case
+  V.EvKey k ms -> EvKey k (Set.fromList ms)
+  V.EvMouseDown r c b ms -> EvMouseDown (V2 (Sum r) (Sum c)) b (Set.fromList ms)
+  V.EvMouseUp r c _ -> EvMouseUp (V2 (Sum r) (Sum c))
+  V.EvResize r c -> EvResize (V2 (Sum r) (Sum c))
+  V.EvPaste bs -> EvPaste (TE.decodeUtf8 bs)
+  V.EvLostFocus -> EvLostFocus
+  V.EvGainedFocus -> EvGainedFocus
+
+-- newtype W a = W { unW ::
+
 data Widget m e n v = Widget
   { widgetBox :: !(Box n)
-  , widgetCallback :: !(e -> m ())
+  , widgetCallback :: !(V2 n -> e -> m ())
   , widgetView :: !(m v)
   }
 
@@ -377,10 +410,145 @@ instance Monoid n => HasSize n (Widget m e n v) where
   getSize = getSize . widgetBox
 
 widgetPure :: (Applicative m, Monoid n, HasSize n v) => v -> Widget m e n v
-widgetPure v = Widget (box (getSize v)) (const (pure ())) (pure v)
+widgetPure v = Widget (box (getSize v)) (\_ _ -> pure ()) (pure v)
 
--- instance (Applicative m, HasSize n v) => Stackable (Widget m e n v) where
---   stack l (Widget b1 cb1 v1) (Widget b2 cb2 v2) = Widget b cb v where
+instance (Applicative m, SizeLike n, HasSize n v, HasFill n b, Stackable b v) => Stackable b (Widget m e n v) where
+  stack ly bo (Widget bx1 cb1 v1) (Widget bx2 cb2 v2) = Widget bx cb v
+   where
+    (bx1', bx2') = widen ly bx1 bx2
+    bx = stack ly bo bx1' bx2'
+    diff =
+      let z = getFill bo
+      in  case ly of
+            LayoutHoriz -> V2 (invert (getWidth bx1' <> z)) mempty
+            LayoutVert -> V2 mempty (invert (getHeight bx1' <> z))
+    cb p e =
+      if hitBox p bx1'
+        then cb1 p e
+        else let p' = p <> diff in when (hitBox p bx2') (cb2 p' e)
+    v = liftA2 (stack ly bo) v1 v2
 
 -- buildW :: (Applicative m, HasSize n v) => Frame n (Widget m e n v) -> Widget m e n v
 -- buildW = build onBorder onContent where
+
+newtype Id = Id {unId :: Int}
+  deriving stock (Show)
+  deriving newtype (Eq, Ord, Enum)
+
+data Ref n = Ref {refId :: !Id, refOffset :: !(V2 n)}
+  deriving stock (Eq, Ord, Show)
+
+type IdFrame n b z = FrameF n (Elem b (Ref n) z)
+
+data Focus n = Focus
+  { focId :: !Id
+  , focCursor :: !(V2 n)
+  }
+  deriving stock (Eq, Ord, Show)
+
+data Camera n = Camera
+  { camRoot :: !Id
+  , camFocus :: !(Maybe (Focus n))
+  }
+  deriving stock (Eq, Ord, Show)
+
+data St n b z = St
+  { stNextId :: !Id
+  , stFrames :: !(Map Id (IdFrame n b z))
+  -- , stCamera :: !(Maybe (Camera n))
+  }
+  deriving stock (Eq, Ord, Show)
+
+newtype M n b z a = M {unM :: State (St n b z) a}
+  deriving newtype (Functor, Applicative, Monad, MonadState (St n b z))
+
+runM :: M n b z a -> St n b z -> (a, St n b z)
+runM = runState . unM
+
+addFrameF :: IdFrame n b z -> M n b z Id
+addFrameF r =
+  state (\(St k m) -> (k, St {stNextId = succ k, stFrames = Map.insert k r m}))
+
+foldState :: (s -> a -> (b, s)) -> s -> Seq a -> (Seq b, s)
+foldState f = go Empty
+ where
+  go !acc !s = \case
+    Empty -> (acc, s)
+    a :<| as -> let (b, s') = f s a in go (acc :|> b) s' as
+
+addOffset :: (Group n, HasFill n b) => FrameF n (Elem b Id z) -> IdFrame n b z
+addOffset (FrameF bx c) = FrameF bx (goElem c)
+ where
+  goElem = \case
+    ElemPart z -> ElemPart z
+    ElemLayout ly bo r rs ->
+      ElemLayout ly bo (Ref r mempty) (fst (foldState (goFold (layoutOffset bx ly bo)) mempty rs))
+  goFold layOff off i = let off' = off <> layOff in (Ref i off', off')
+
+addFrame :: (Group n, HasFill n b) => Frame n b z -> M n b z Id
+addFrame = frameCataM (addFrameF . addOffset)
+
+lookupFrame :: Id -> M n b z (Maybe (IdFrame n b z))
+lookupFrame i = gets (Map.lookup i . stFrames)
+
+newtype X m a = X {unX :: StateT (Set Id) (MaybeT m) a}
+  deriving newtype (Functor, Applicative, Monad, MonadState (Set Id), Alternative)
+
+instance MonadTrans X where
+  lift = X . lift . lift
+
+runX :: X m a -> Set Id -> m (Maybe (a, Set Id))
+runX x s = runMaybeT (runStateT (unX x) s)
+
+evalX :: Functor m => X m a -> m (Maybe a)
+evalX x = fmap (fmap fst) (runX x Set.empty)
+
+extractFrame :: Id -> M n b z (Maybe (Frame n b z))
+extractFrame = fmap (fmap Frame) . evalX . go
+ where
+  go i = do
+    mjr <- lift (lookupFrame i)
+    case mjr of
+      Nothing -> empty
+      Just r -> do
+        seen <- get
+        if Set.member i seen
+          then empty
+          else do
+            modify' (Set.insert i)
+            traverse (bitraverse (fmap Frame . go . refId) pure) r
+
+-- hitFrameM :: (Semigroup n, Ord n) => V2 n -> M n b z (Maybe (Id, IdFrame n b z))
+-- hitFrameM pos = runMaybeT goRoot where
+--   goRoot = do
+--     q <- MaybeT (gets (fmap camRoot . stCamera))
+--     goSearch Nothing (Seq.singleton q)
+--   goSearch best = \case
+--     Empty -> maybe empty pure best
+--     q :<| rest -> do
+--       r@(FrameF bx c) <- MaybeT (lookupFrame q)
+--       let p = (q, r)
+--       if hitBox pos bx
+--         then case c of
+--           ElemPart _ -> pure p
+--           ElemLayout _ _ s ss -> goSearch (Just p) (s :<| (ss <> rest))
+--         else goSearch (Just p) rest
+
+-- data Dir = DirLeft | DirRight | DirUp | DirDown
+--   deriving stock (Eq, Ord, Show)
+
+-- keyDir :: V.Key -> Maybe Dir
+-- keyDir = \case
+--   V.KLeft -> Just DirLeft
+--   V.KRight -> Just DirRight
+--   V.KUp -> Just DirUp
+--   V.KDown -> Just DirDown
+--   _ -> Nothing
+
+-- -- dirTargetFrame :: Dir -> M n b z (Maybe (Id, IdFrame n b z))
+-- -- dirTargetFrame dir = runMaybeT goRoot where
+-- --   goRoot = do
+-- --     foc <- MaybeT (gets (camFocus <=< stCamera))
+-- --     undefined
+--     -- r <- MaybeT (lookupFrame q)
+--     -- goSearch Nothing (Seq.singleton (q, r))
